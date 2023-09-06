b52031a7ed019de7e3d9505b3f6af85d54daf1a4
/home/mdalifbabu/Alif/C#/dapperAngular <alif.babu@digigate360.com>
2023-09-06 18:17:56 +0600

    securing web by jwt with role

---
+++ src/test/java/com/example/springrolejwt/SpringRoleJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrolejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRoleJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
